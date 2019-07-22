1b163a8c09b9b823d4f8a399cbaf038e9a4aa5e6
kaandinler <kaan_d_tr@hotmail.com>
2019-07-22 11:00:40 +0300

    asd

---
+++ index.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>KaanDinler.com - Personal Web Site</title>
+
+	<meta charset="utf-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- favicon -->
+	<link rel="icon" type="image/png" href="image/favicon.png">
+
+	<!-- CSS -->
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/kaandinler.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+
+	<!--  -->
+
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Raleway:300,300i,400,500,600i,700|Source+Sans+Pro:300,300i,400,600,600i&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+
+	<header class="header">
+		
+		<div class="header-overlay">
+			<div class="container">
+				
+				<div class="row">
+					
+					<div class="col-md-12">
+						<!-- logo -->
+
+						<div class="logo text-center">
+							<img src="image/logo.png" alt="logo"/>
+						</div> <!-- .logo -->
+
+					</div> <!-- col-md-12 -->
+
+				</div> <!-- .row -->
+
+			<div class="row">
+				
+				<div class="col-md-8">
+					
+					<div class="header-text">
+						<!-- title and description -->
+						<h1>Responsive and modern sites as your need</h1>
+						<p>Fits your needsw</p>
+					</div> <!-- .header-text -->
+
+					<div class="header-btns">
+						<!-- header buttons -->
+						<a href="#" class="btn btn-contact">
+							Contact
+						</a>
+						<a href="#" class="btn btn-tour">		Take A Tour
+							<i class="fa fa-angle-down"></i>
+						</a>
+						
+					</div>
+
+				</div> <!-- .col-md-8 -->
+
+				<div class="col-md-3 offset-md-1">
+					
+					<div class="header-devices">
+						<!-- header devices -->
+						<img src="image/iphone-header.png" alt="header devices">
+					</div>
+
+				</div><!-- .col-md-4 -->
+
+			</div> <!-- .row -->
+
+			</div> <!-- .container -->
+
+		</div> <!-- .header-overlay -->
+
+	 </header>  <!-- HEADER -->
+	
+	<!-- About Us -->
+	<section>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 offset-md-1">
+					<!-- about us title and description -->
+					<div class="section-title">
+						<h2>About Me</h2>
+						<p>Hi! my name is kaan and I'm a computer engineering student at Eskişehir Technical University. Computers are my life! If you need an Android App or a responsive web site, you can contact with me.</p>
+					</div> <!-- .section-title -->
+				</div><!-- col-md-10 col-md-offset-1 -->
+			</div> <!-- .row -->
+		</div> <!-- .container -->
+
+		<div class="choose-me-wrapper">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						<!-- about web -->
+						<div class="about-web">
+							<img src="image/about-iphone.png" alt="web">
+						</div> <!-- .about-web -->
+					</div><!-- .col-md-6 -->
+					<div class="col-md-6">
+						<!-- Why choose me -->
+						<div class="choose-me-title">
+							<h2>Why Choose Me?</h2>
+							<p>WHY CHOOSE ME CONTENT WHY CHOOSE ME CONTENTWHY CHOOSE ME CONTENT</p>
+						</div><!-- .choose-me-title -->
+						<div class="choose-me-des">
+
+							<!-- choose me item 1 -->
+							<div class="choose-me-item">
+								<!-- choose me item icon -->
+								<div><i class="fa fa-bolt"></i></div>
+								<!-- choose me item title & des -->
+								<h3>BAŞLIK BAŞLIK BAŞLIK</h3>
+								<p>İÇERİK İÇERİK İÇERİK İÇERİK</p>
+							</div> <!-- .choose-me-item -->
+
+							<!-- choose me item 2 -->
+							<div class="choose-me-item">
+								<!-- choose me item icon -->
+								<div><i class="fa fa-bullhorn"></i></div>
+								<!-- choose me item title & des -->
+								<h3>BAŞLIK BAŞLIK BAŞLIK</h3>
+								<p>İÇERİK İÇERİK İÇERİK İÇERİK</p>
+							</div> <!-- .choose-me-item -->
+
+							<!-- choose me item 3 -->
+							<div class="choose-me-item">
+								<!-- choose me item icon -->
+								<div><i class="fa fa-flag-o"></i></div>
+								<!-- choose me item title & des -->
+								<h3>BAŞLIK BAŞLIK BAŞLIK</h3>
+								<p>İÇERİK İÇERİK İÇERİK İÇERİK</p>
+							</div> <!-- .choose-me-item -->
+
+						</div> <!-- .choose-me-des -->
+					</div> <!-- .col-md-6 -->
+				</div><!-- .row -->
+			</div><!--  .container -->
+		</div><!-- .choose-me-wrapper -->
+
+	</section>
+
+	<!-- Testimonials -->
+	<section  class="testimonial">
+		<div class="testimonial-overlay">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div id="carousel-testimonial" class="carousel slide" data-ride="carousel">
+							<!-- indicators -->
+							<ol class="carousel-indicators">
+								<li data-target="#carousel-testimonial" data-slide-to="0" class="active"></li>
+								<li data-target="#carousel-testimonial" data-slide-to="1"></li>
+								<li data-target="#carousel-testimonial" data-slide-to="2"></li>
+							</ol>
+							<!-- wrapper for slides -->
+							<div class="carousel-inner">
+								<!-- item 1 -->
+								<div class="carousel-item active text-center">
+									<img src="image/client-01.jpg" alt="testimonial" class="center-block">
+									<div class="testimonial-caption">
+										<h2>Kaan</h2>
+										<h4><span>Bilgisayar Mühendisi</span>
+										Freelance</h4>
+										<p>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir. Lorem Ipsum kullanmanın amacı, sürekli 'buraya metin gelecek, buraya metin gelecek' yazmaya kıyasla daha dengeli bir harf dağılımı sağlayarak okunurluğu artırmasıdır. Şu anda birçok masaüstü yayıncılık paketi ve web sayfa düzenleyicisi, varsayılan mıgır metinler olarak Lorem Ipsum kullanmaktadır. Ayrıca arama motorlarında 'lorem ipsum' anahtar sözcükleri ile arama yapıldığında henüz tasarım aşamasında olan çok sayıda site listelenir. Yıllar içinde, bazen kazara, bazen bilinçli olarak (örneğin mizah katılarak), çeşitli sürümleri geliştirilmiştir.</p>
+									</div> <!-- .testimonial-caption -->
+								</div> <!-- .item active text-center -->
+
+								<!-- item 2 -->
+								<div class="carousel-item text-center">
+									<img src="image/client-02.jpg" alt="testimonial" class="center-block">
+									<div class="testimonial-caption">
+										<h2>Kaan</h2>
+										<h4><span>Bilgisayar Mühendisi</span>
+										Freelance</h4>
+										<p>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir. Lorem Ipsum kullanmanın amacı, sürekli 'buraya metin gelecek, buraya metin gelecek' yazmaya kıyasla daha dengeli bir harf dağılımı sağlayarak okunurluğu artırmasıdır. Şu anda birçok masaüstü yayıncılık paketi ve web sayfa düzenleyicisi, varsayılan mıgır metinler olarak Lorem Ipsum kullanmaktadır. Ayrıca arama motorlarında 'lorem ipsum' anahtar sözcükleri ile arama yapıldığında henüz tasarım aşamasında olan çok sayıda site listelenir. Yıllar içinde, bazen kazara, bazen bilinçli olarak (örneğin mizah katılarak), çeşitli sürümleri geliştirilmiştir.</p>
+									</div> <!-- .testimonial-caption -->
+								</div> <!-- .item active text-center -->
+
+								<!-- item 3 -->
+								<div class="carousel-item text-center">
+									<img src="image/client-03.jpg" alt="testimonial" class="center-block">
+									<div class="testimonial-caption">
+										<h2>Kaan</h2>
+										<h4><span>Bilgisayar Mühendisi</span>
+										Freelance</h4>
+										<p>Yinelenen bir sayfa içeriğinin okuyucunun dikkatini dağıttığı bilinen bir gerçektir. Lorem Ipsum kullanmanın amacı, sürekli 'buraya metin gelecek, buraya metin gelecek' yazmaya kıyasla daha dengeli bir harf dağılımı sağlayarak okunurluğu artırmasıdır. Şu anda birçok masaüstü yayıncılık paketi ve web sayfa düzenleyicisi, varsayılan mıgır metinler olarak Lorem Ipsum kullanmaktadır. Ayrıca arama motorlarında 'lorem ipsum' anahtar sözcükleri ile arama yapıldığında henüz tasarım aşamasında olan çok sayıda site listelenir. Yıllar içinde, bazen kazara, bazen bilinçli olarak (örneğin mizah katılarak), çeşitli sürümleri geliştirilmiştir.</p>
+									</div> <!-- .testimonial-caption -->
+								</div> <!-- .item active text-center -->
+							</div> <!-- .carousel-inner -->
+						</div> <!-- .carousel slide -->
+					</div> <!-- .col-md-12 -->
+				</div> <!-- .row -->
+			</div>	<!-- .container -->
+		</div> <!-- .testimonial-overlay -->
+	</section>
+
+	<!-- Features -->
+	<section class="features">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 offset-md-1">
+					<div class="section-title">
+						<h2>Features</h2>
+						<p>nde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit </p>
+					</div> <!-- .section-title -->
+				</div><!-- .col-md-10 offset-md-1 -->
+			</div>
+		</div>
+		<div class="features-inner-wrapper">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						
+						<!-- left feature item 1 -->
+						<div class="left-feature-item">
+							<div class="left-feature-item-icon">
+								<i class="fa fa-desktop"></i>
+							</div><!-- .left-feature-item-icon -->
+							<!-- feature item title & description -->
+							<h3>BAŞLIK</h3>
+							<p>İÇERİK İÇERİK İÇERİK İÇERİK İÇERİK İÇERİK İÇERİK İÇERİK İÇERİK</p>
+						</div><!-- .left-feature-item -->
+
+						<!-- left feature item 2-->
+						<div class="left-feature-item">
+							<div class="left-feature-item-icon">
+								<i class="fa fa-mobile"></i>
+							</div><!-- .left-feature-item-icon -->
+							<!-- feature item title & description -->
+							<h3>BAŞLIK</h3>
+							<p>İÇERİK İÇERİK İÇERİK İÇERİK İÇERİK İÇERİK İÇERİK İÇERİK İÇERİK</p>
+						</div><!-- .left-feature-item -->
+
+						<!-- left feature item 3 -->
+						<div class="left-feature-item">
+							<div class="left-feature-item-icon">
+								<i class="fa fa-eye"></i>
+							</div><!-- .left-feature-item-icon -->
+							<!-- feature item title & description -->
+							<h3>BAŞLIK</h3>
+							<p>İÇERİK İÇERİK İÇERİK İÇERİK İÇERİK İÇERİK İÇERİK İÇERİK İÇERİK</p>
+						</div><!-- .left-feature-item -->
+
+					</div><!-- .col-md-4 -->
+
+					<div class="col-md-4">
+						<div class="feature-device">
+							<img src="image/iphone-02.png" alt="metin">
+						</div>
+					</div><!--  .col-md-4 -->
+
+					<div class="col-md-4">
+
+						<!-- right featured item 1-->
+						<div class="right-feature-item">
+							<!-- icon -->
+							<div class="right-feature-item-icon">
+								<i class="fa fa-certificate"></i>
+							</div> <!-- .right-feature-item-icons -->
+							<!-- featured item title & description -->
+							<h3>BAŞLIK BAŞLIK</h3>
+							<P>içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik </P>
+						</div> <!-- .right-feature-item -->
+
+						<!-- right featured item 2-->
+						<div class="right-feature-item">
+							<!-- icon -->
+							<div class="right-feature-item-icon">
+								<i class="fa fa-rss"></i>
+							</div> <!-- .right-feature-item-icons -->
+							<!-- featured item title & description -->
+							<h3>BAŞLIK BAŞLIK</h3>
+							<P>içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik </P>
+						</div> <!-- .right-feature-item -->
+						
+						<!-- right featured item 3-->
+						<div class="right-feature-item">
+							<!-- icon -->
+							<div class="right-feature-item-icon">
+								<i class="fa fa-database"></i>
+							</div> <!-- .right-feature-item-icons -->
+							<!-- featured item title & description -->
+							<h3>başlık BAŞLIK</h3>
+							<P>içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik içerik </P>
+						</div> <!-- .right-feature-item -->
+						
+					</div> <!-- .col-md-4 -->
+
+				</div><!-- .row -->
+			</div><!-- .container -->
+		</div><!-- .features-inner-wrapper -->
+	</section> <!-- .features -->
+
+	<script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
